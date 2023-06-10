da7345cb2ab8212640c8f8fce2124fd71140f714
aryans0714 <74842047+aryans0714@users.noreply.github.com>
2023-06-09 22:31:47 -0400

    Update index.js

--- screens/index.js
+++ screens/index.js
@@ -5,3 +5,7 @@ export { default as HomeScreen } from './HomeScreen/HomeScreen'
 export { default as RegistrationScreen } from './RegistrationScreen/RegistrationScreen'
 
 export { default as PrScreen } from './PrScreen/PrScreen'
+
+export { default as AiScreen } from './AiScreen/AiScreen'
+
+export { default as AccountScreen } from './AccountScreen/AccountScreen'
