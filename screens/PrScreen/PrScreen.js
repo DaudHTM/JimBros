5f79d4cbcf8610cbb8eb74688ecd94890f204e8b
DaudHTM <DaudHTM@gmail.com>
2023-06-12 19:05:38 -0400

    added modal

--- screens/PrScreen/PrScreen.js
+++ screens/PrScreen/PrScreen.js
@@ -2,13 +2,22 @@ import React, { useState, useEffect } from 'react'
 import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import styles from './styles'
 import { firebase } from '../../assets/src/firebase/config'
+import {AddExerciseModal} from "./addExerciseModal/addExerciseModal"
 
 export default function PrScreen({ navigation, userData }) {
   const [prArr, setPrArr] = useState([])
   const uid = userData.id
   const prRef = firebase.firestore().collection('users').doc(uid).collection('pr')
+  const [toggleModal,setToggleModal] = useState(false);
+
+
+  const toggleModalFunction = () => {
+    setToggleModal(!toggleModal);
+  }
+
 
   useEffect(() => {
+ 
     prRef
 
       .onSnapshot(
@@ -27,8 +36,18 @@ export default function PrScreen({ navigation, userData }) {
   }, [])
 
   return (
-    <View>
-      <Text>{prArr.id}</Text>
+    <View style={styles.screenContainer}>
+      
+
+{toggleModal ? <AddExerciseModal userData={userData}/> : null}
+      
+
+   
+      <TouchableOpacity style={styles.addExerciseButton} onPress={toggleModalFunction}><Text>Add Exercise</Text></TouchableOpacity>
+
+     
     </View>
   )
 }
+
+
