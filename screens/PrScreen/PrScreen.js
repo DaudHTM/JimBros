5a331141b4d997831432a2a3f2cc0717355afec0
aryans0714 <aryan.sharma0714@gmail.com>
2023-06-13 15:34:37 -0400

    fixed atrocious pr screen pt1

--- screens/PrScreen/PrScreen.js
+++ screens/PrScreen/PrScreen.js
@@ -37,15 +37,29 @@ export default function PrScreen({ navigation, userData }) {
 
   return (
     <View style={styles.screenContainer}>
-      
+    {toggleModal ? <AddExerciseModal userData={userData}/> : null}
+    <TouchableOpacity style={styles.logButton} onPress={toggleModalFunction}>
+      <Text style={styles.logButtonText}>📝 Log Workout</Text>
+    </TouchableOpacity>
 
-{toggleModal ? <AddExerciseModal userData={userData}/> : null}
-      
+    <TouchableOpacity style={styles.addPrButton}>
+      <Text style={styles.addPrButtonText}>🏅 Add PR!</Text>
+    </TouchableOpacity>
 
-   
-      <TouchableOpacity style={styles.addExerciseButton} onPress={toggleModalFunction}><Text>Add Exercise</Text></TouchableOpacity>
+    <TouchableOpacity style={styles.modalContainer} onPress={() => alert('To be added')}>
+      <Text style={styles.modalTitle}>Workout Log</Text>
+      <Text style={styles.modalText}>
+        You hit 5x10 deadlifts of 225
+        {'\n'}
+        You did 3x10 squats of 135
+        {'\n'}
+        You hit a new PR! 🔥
+        {'\n'}
+        Click to read more...
+      </Text>
+    </TouchableOpacity>
 
-     
+    <View style={styles.graphContainer}>{/* Add your square graph component here */}</View>
     </View>
   )
 }
