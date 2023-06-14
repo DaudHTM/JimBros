8b96bb131797e04f9b3d415a493a7b4ee02dd598
DaudHTM <DaudHTM@gmail.com>
2023-06-14 00:27:11 -0400

    merge fix

--- screens/PrScreen/styles.js
+++ screens/PrScreen/styles.js
@@ -1,62 +1,81 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  screenContainer: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: 'white',
-  },
-  logButton: {
-    width: '90%',
-    borderRadius: 10,
-    height: 60,
-    backgroundColor: '#788eec',
-    marginBottom: 20,
-    marginTop: 20,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  logButtonText: {
-    color: 'white',
-    fontSize: 18,
-    fontWeight: 'bold'
-  },
-  addPrButton: {
-    width: '90%',
-    borderRadius: 10,
-    height: 60,
-    backgroundColor: '#ffd700',
-    marginBottom: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  addPrButtonText: {
-    color: 'black',
-    fontSize: 18,
-    fontWeight: 'bold',
-    textShadowColor: 'orange', // Orange text glow
-    textShadowOffset: { width: 0, height: 0 },
-    textShadowRadius: 5,
-  },
-  modalContainer: {
-    backgroundColor: '#777',
-    borderRadius: 10,
-    padding: 20,
-    width: '90%',
-    position: 'relative',
-    alignItems: "left",
-  },
-  modalTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    color: "#1f1f1f",
-  },
-  modalText: {
-    fontSize: 16,
-    fontStyle: 'italic',
-    color: "#000",
-  },
-  graphContainer: {
-  },
-});
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    formContainer: {
+        flexDirection: 'row',
+        height: 80,
+        marginTop: 40,
+        marginBottom: 20,
+        flex: 1,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 30,
+        paddingRight: 30,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    input: {
+        height: 48,
+        borderRadius: 5,
+        overflow: 'hidden',
+        backgroundColor: 'white',
+        paddingLeft: 16,
+        flex: 1,
+        marginRight: 5
+    },
+    button: {
+        height: 47,
+        borderRadius: 5,
+        backgroundColor: '#788eec',
+        width: 80,
+        alignItems: "center",
+        justifyContent: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16
+    },
+    listContainer: {
+        marginTop: 20,
+        padding: 20,
+    },
+    entityContainer: {
+        marginTop: 16,
+        borderBottomColor: '#cccccc',
+        borderBottomWidth: 1,
+        paddingBottom: 16
+    },
+    entityText: {
+        fontSize: 20,
+        color: '#333333'
+    },
+    screenContainer:{
+        top:0,
+        left:0,
+        width:'100%',
+        height:'100%',
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'white',
+
+    },
+    addExerciseButton:{
+        position:'absolute',
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center',
+        width:190,
+        borderRadius:10,
+        height:60,
+        backgroundColor: '#788eec',
+        bottom:25,
+        flex:1,
+    }
+
+})
