d8f906de6191035b499c8db8dba530357c774c7d
DaudHTM <DaudHTM@gmail.com>
2023-06-15 23:30:19 -0400

    fixed css added socail screen

---
+++ screens/AiScreen/SocialScreen.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import styles from './styles';
+
+export default function SocialScreen({ navigation, userData} ) {
+
+
+  return (
+    <View>
+      <Text>AI Screen</Text>
+    </View>
+  );
+}
