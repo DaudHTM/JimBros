8c0e9b3e243f966ff2396a573f9cdb5c1f510e3a
aryans0714 <aryan.sharma0714@gmail.com>
2023-06-17 15:55:25 -0400

    mb

---
+++ screens/PrScreen/viewWorkoutHistory/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    alignItems: "center",
+    height: "100%",
+    width: "100%",
+    backgroundColor: "#777"
+  }
+});
---
+++ screens/PrScreen/viewWorkoutHistory/viewWorkoutHistory.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import styles from "./style";
+
+export default function viewWorkoutHistory({ userData, closeModal }) {
+  return (
+    <View style={styles.container}>
+        <Text>A</Text>
+    </View>
+  );
+}
+
+export { viewWorkoutHistory };
